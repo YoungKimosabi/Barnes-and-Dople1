8d24abadf55b99fb890d1c189fc73e910a7888d0
Roberto Hasfura <robertohasfura@Robertos-MacBook-Air.local>
2022-05-31 17:02:48 -0500

    updated some items on NoAuth

--- src/api/apiNoAuth.js
+++ src/api/apiNoAuth.js
@@ -2,15 +2,15 @@ import apiClientNoAuth from './clientNoAuth';
 
 const endpoint = '/user';
 
-export const postUser = async (email, first_name, last_name, password, cancelToken) => {
+export const postUser = async (data, cancelToken) => {
     let error;
     let user;
 
-    const response = await apiClientNoAuth(email, first_name, last_name, password, cancelToken).get(endpoint);
+    const response = await apiClientNoAuth(cancelToken).post(endpoint, data);
     if (response.ok){
         user = response.data
-    }else if (response.status === 401){
-        error="You must coplete all form fields to proceed"
+    }else if (response.status === 422){
+        error="This email address is already taken. Please user another email"
     }else{
         error = "An unexpected error has occurred. Please try again."
     }
